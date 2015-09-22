17b3d326d29dc1f5437ce9d5c9a0aea1db9a9284
Christian Gill <gillchristiang@gmail.com>
2015-09-22 09:08:03 -0300

    Fixed tooltip event! :ghost:

--- app/js/directives/tooltipEvents.js
+++ app/js/directives/tooltipEvents.js
@@ -6,16 +6,15 @@
 		    return {
 		        restrict: "A",
 		        scope: {
-		        	triggerTooltip: '='
+		        	check: '&triggerTooltip'
 		        },
 		        link: function (scope, element, attrs, ctrl) {
 
         			//console.log(angular.element(element[0]));
 
 		        	angular.element(element[0]).on('blur', function(e){
-		        		console.log('true?',scope.triggerTooltip);
-		        		if(scope.triggerTooltip) {
-	        				element[0].dispatchEvent(new Event('customEvent'));
+		        		if(scope.check) {
+	        				element[0].dispatchEvent(new Event('tooltipActivate'));
 		        		}
 		        	});
 
@@ -23,22 +22,6 @@
 		        	element.find('input').on('tooltipActivate', function(e){
 	        			console.log('triggered');
 		        	});
-/*
-		            element.find('input').on('blur', function () {
-			        	if (scope.triggerTooltip){
-			                element.find('input').triggerHandler('tooltipActivate');
-			        	}
-		            });
-		            */
-		            /*scope.$on('blur', function(){
-		            	if(scope.triggerTooltip){
-		            		scope.$emit('tooltipActivate');
-		            	}
-		            });
-
-		            scope.$on('tooltipActivate', function(){
-		            	console.log('triggered');
-		            });*/
 		        }
 		    };
 	});
